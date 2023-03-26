20c1cda8b8b3f6f65da8281b7eebbdd13f91dc09
ezitounioussama <zitounioussama95@gmail.com>
2023-03-26 16:58:02 +0000

    fixing some annoying warnings

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Announcement = () => {
   return (
-    <div class="bg-indigo-600 px-4 py-3 text-white">
-      <div class="text-center text-sm font-medium">
-        <p class="inline-block underline">Check out our new stunning movies!</p>
+    <div className="bg-indigo-600 px-4 py-3 text-white">
+      <div className="text-center text-sm font-medium">
+        <p className="inline-block underline">
+          Check out our new stunning movies!
+        </p>
       </div>
     </div>
   );
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -8,8 +8,8 @@ function Filter({
 }) {
   return (
     <div className="flex justify-evenly items-center my-5">
-      <div class="hidden sm:flex sm:gap-4">
-        <div class="relative">
+      <div className="hidden sm:flex sm:gap-4">
+        <div className="relative">
           <input
             type="range"
             min="5"
@@ -29,13 +29,13 @@ function Filter({
         </div>
       </div>
 
-      <div class="relative">
-        <label class="sr-only" for="search">
+      <div className="relative">
+        <label className="sr-only" htmlFor="search">
           Search
         </label>
 
         <input
-          class="h-10 w-full rounded-full border-none bg-white pl-4 pr-10 text-sm shadow-sm sm:w-56"
+          className="h-10 w-full rounded-full border-none bg-white pl-4 pr-10 text-sm shadow-sm sm:w-56"
           id="search"
           type="search"
           placeholder="Search Title..."
@@ -45,20 +45,20 @@ function Filter({
 
         <button
           type="button"
-          class="absolute top-1/2 right-1 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
+          className="absolute top-1/2 right-1 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
         >
-          <span class="sr-only">Search</span>
+          <span className="sr-only">Search</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4"
+            className="h-4 w-4"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
